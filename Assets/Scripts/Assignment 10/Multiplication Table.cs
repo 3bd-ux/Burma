e683eaf127132842781a81dc9f3bae30278f38e4
abed ebrahem <abed-1997@hotmail.com>
2024-11-01 07:54:32 +0200

    Multiplication Table Assignement.

--- Assets/Scripts/Assignment 10/Multiplication Table.cs
+++ Assets/Scripts/Assignment 10/Multiplication Table.cs
@@ -8,20 +8,25 @@ public class MultiplicationTable : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //string 
-        int temp = 0;
+        string RESULT = null;
+
         if (number == 0) number = 5;
+
         for (int i = 1; i <= 10; i++)
         {
-            Debug.Log($"|{Multiplier(number, i)}");
-            temp = i;
+            RESULT += i + " X " + number + " = " + (Multiply(i, number));
+            Debug.Log(Multiply(i, number));
+            if(i==10)continue;
+            RESULT +=",   ";
         }
-
-        // Debug.Log(number + " * " + temp + " = " + Multiplier(number, temp));
+        Debug.Log(RESULT);
         Debug.LogError("Donzos");
+
+        // Debug.Log($"|{Multiply(number, i)}");
+        // Debug.Log(number + " * " + temp + " = " + Multiplier(number, temp));
     }
 
-    private int Multiplier(int num1, int num2)
+    private int Multiply(int num1, int num2)
     {
         return num1 * num2;
     }
