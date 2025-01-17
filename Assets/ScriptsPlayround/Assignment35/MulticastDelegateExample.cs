415dcdc80b8587412636521149fad4ff79515af6
abed ebrahem <abed-1997@hotmail.com>
2025-01-17 19:22:27 +0200

    coverted some to Lambda

--- Assets/ScriptsPlayround/Assignment35/MulticastDelegateExample.cs
+++ Assets/ScriptsPlayround/Assignment35/MulticastDelegateExample.cs
@@ -4,26 +4,9 @@ namespace Assignment35
     public class MulticastDelegateExample : MonoBehaviour
     {
         delegate void MathOperation(int number);
-
-        void DoubleNumber(int number)
-        {
-            number *= 2;
-            Debug.Log("Double: " + number);
-        }
-
-        void SquareNumber(int number)
-        {
-            number *= number;
-            Debug.Log("Square: " + number);
-        }
-
-
-        void CubeNumber(int number)
-        {
-            number *= number * number;
-            Debug.Log("Cube: " + number);
-        }
-
+        void DoubleNumber(int number) => Debug.Log("Double: " + (number *= 2));
+        void SquareNumber(int number) => Debug.Log("Square: " + (number *= number));
+        void CubeNumber(int number) => Debug.Log("Cube: " + (number = number * number * number));
         void Start()
         {
             MathOperation operation = null;
